3960884e7db4bdf639a67c3ee198bae23ff11938
sasha <savylovskij@gmail.com>
2023-07-30 19:02:32 +0300

    backend: add mongo

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { TrackModule } from './track/track.module';
 
 @Module({
-  imports: [TrackModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.kkm7otg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    ),
+    TrackModule,
+  ],
 })
 export class AppModule {}
