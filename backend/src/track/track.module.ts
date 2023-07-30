8756f7c6273f885f30e6c679739ccd34390c7192
sasha <savylovskij@gmail.com>
2023-07-30 19:54:31 +0300

    backend: add schemas

--- backend/src/track/track.module.ts
+++ backend/src/track/track.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { TrackController } from './track.controller';
 import { TrackService } from './track.service';
+import { Comment, CommentSchema, Track, TrackSchema } from './schemas';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Track.name,
+        schema: TrackSchema,
+      },
+      {
+        name: Comment.name,
+        schema: CommentSchema,
+      },
+    ]),
+  ],
   controllers: [TrackController],
   providers: [TrackService],
 })
--- backend/src/track/track.service.ts
+++ backend/src/track/track.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Comment, Track } from './schemas';
 
 @Injectable()
 export class TrackService {
+  constructor(
+    @InjectModel(Track.name) private readonly trackModel: Model<Track>,
+    @InjectModel(Comment.name) private readonly commentModel: Model<Comment>,
+  ) {}
+
   async create() {}
   async getAll() {}
   async getOne() {}
