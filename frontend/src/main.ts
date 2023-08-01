5b248ad4c559d3d4be5d05f995740f5958d1fbe9
Sasha <savylovskij@gmail.com>
2023-08-01 22:58:21 +0300

    frontend: first commit

---
+++ frontend/src/main.ts
@@ -0,0 +1,10 @@
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
+})
+  .catch(err => console.error(err));
