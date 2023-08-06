2c5870d8e7be8bc8479139ef6af8f28d04f1ae09
sasha <savylovskij@gmail.com>
2023-08-06 23:40:16 +0300

    frontend: Add new font. sStarted work with header.

--- frontend/src/app/shared/components/main-layout/components/header/header.component.ts
+++ frontend/src/app/shared/components/main-layout/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgOptimizedImage, RouterLink],
 })
 export class HeaderComponent {}
